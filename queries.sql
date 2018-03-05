50e9c83c0137d21d59fedf5b61834a79d6b3abe7
Sarah Morris <sarah.morris9@gmail.com>
2018-03-05 09:42:42 -0600

    Started exercise, not done

---
+++ queries.sql
@@ -0,0 +1,26 @@
+(1) SELECT * FROM Genre;
+
+(2) INSERT INTO "Artist" ("ArtistName", "YearEstablished")
+ VALUES ("John Mayer", "1999");
+
+(3) INSERT INTO "Album" ("Title", "ReleaseDate", "AlbumLength", "Label", "GenreId")
+ VALUES ("Room For Squares", "2001", 3436, "Columbia", 2);
+
+(4) INSERT INTO "Song" ("Title", "SongLength", "ReleaseDate")
+ VALUES ("No Such Thing", 0351, "2001");
+ INSERT INTO "Song" ("Title", "SongLength", "ReleaseDate")
+ VALUES ("St. Patrick's Day", 0520, "2001");
+
+(5) 
+
+(6)
+
+(7)
+
+(8)
+
+(9)
+
+(10)
+
+(11)
